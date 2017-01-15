f4f45e5b9a337c0cbc6a134a9fefd4ef65f0d02e
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:30:32 +1100

    NO CHANGE RECORD

--- ANN/ANNXMLconversion.h
+++ ANN/ANNXMLconversion.h
@@ -3,7 +3,7 @@
  * File Name: ANNXMLconversion.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2012 Baxter AI (baxterai.com)
  * Project: Advanced Neural Network (ANN)
- * Project Version: 3a7b 09-June-2012
+ * Project Version: 3a7d 11-June-2012
  * Comments
  *
  *******************************************************************************/
