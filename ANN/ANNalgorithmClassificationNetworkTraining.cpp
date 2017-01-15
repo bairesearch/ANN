d274538dc32628c0d798c4e28e2d6ea236d85d95
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:38:56 +1100

    ANN 3i13b - ANN_ALGORITHM_CLASSIFICATION_NETWORK: verify that ideal values are set after deleting neurons (during prune or merge)

--- ANN/ANNalgorithmClassificationNetworkTraining.cpp
+++ ANN/ANNalgorithmClassificationNetworkTraining.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNalgorithmClassificationNetworkTraining.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 4a13a 14-June-2016
+ * Project Version: 4a13b 14-June-2016
  * Comments:
  *
  *******************************************************************************/
@@ -535,7 +535,8 @@ void deleteNeuron(ANNneuron* frontNeuron, ANNneuron* categoryNeuron, ANNneuron**
 					connectionIter3++;
 				}
 			}
-		}				
+		}
+		updateConnectionIdealValuesParent(frontNeuronBackNeuron, false);				
 	}
 
 	for(vector<ANNneuronConnection*>::iterator connectionIter2 = frontNeuron->frontANNneuronConnectionList.begin(); connectionIter2 != frontNeuron->frontANNneuronConnectionList.end(); connectionIter2++)
@@ -649,8 +650,6 @@ void mergeSimilarNeurons(ANNneuron* categoryNeuron, ANNneuron** firstOutputNeuro
 				
 				deleteNeuron(frontNeuron2, categoryNeuron, firstOutputNeuron);
 				connectionIter2 = categoryNeuron->frontANNneuronConnectionList.erase(connectionIter2);
-				
-				updateConnectionIdealValuesParent(categoryNeuron, false);	//added 4a11b
 			}
 			else
 			{
