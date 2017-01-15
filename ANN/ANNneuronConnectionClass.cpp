1a83ea43ca4ffc9b9137004628871e17dcf764e3
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:38:18 +1100

    NO CHANGE RECORD

--- ANN/ANNneuronConnectionClass.cpp
+++ ANN/ANNneuronConnectionClass.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNANNneuronConnectionClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3h1f 14-November-2015
+ * Project Version: 3h9a 09-December-2015
  * Comments:
  *
  *******************************************************************************/
