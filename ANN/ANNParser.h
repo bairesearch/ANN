db9c7508b89c0a14fa1044dd5f8da3c45f0d9b58
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:30:29 +1100

    NO CHANGE RECORD

--- ANN/ANNParser.h
+++ ANN/ANNParser.h
@@ -3,7 +3,7 @@
  * File Name: ANNParser.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2010 Baxter AI (baxterai.com)
  * Project: Advanced Neural Network (ANN)
- * Project Version: 3a6b 30-Apr-2012
+ * Project Version: 3a6c 07-May-2012
  * Comments: TH = Test Harness
  *
  *******************************************************************************/
