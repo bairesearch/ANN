9bacaa04131710455e95723e8cbf6d402445faab
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:40:38 +1100

    ANN 3i19c - CS compatibility updates - update RTraytracer.h: comment out drawPointTest

--- ANN/ANNbiologicalConstructs.cpp
+++ ANN/ANNbiologicalConstructs.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNbiologicalConstructs.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3i19b 15-December-2016
+ * Project Version: 3i19c 15-December-2016
  * Comments: CONTAINS EXAMPLE CODE AND IS NOT COMPLETE
  *			how to record movement information? (Eg recurrent network required) how to get synthetic network to modify its focus; left, right, in, out towards the objects of most interest?
  *******************************************************************************/
