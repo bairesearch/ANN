db692580fb44cf10a06f124b9d30110424697848
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:38:10 +1100

    NO CHANGE RECORD

--- ANN/ANNdata.cpp
+++ ANN/ANNdata.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNdata.c
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3h1e 14-November-2015
+ * Project Version: 3h1f 14-November-2015
  * Comments: TH = Test Harness
  *
  *******************************************************************************/
