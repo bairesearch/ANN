9c4db4d4aeaad8930a81d2914ebc371c933ea415
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:33:20 +1100

    NO CHANGE RECORD

--- ANN/ANNbiologicalConstructs.cpp
+++ ANN/ANNbiologicalConstructs.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNbiologicalConstructs.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3d4a 08-June-2014
+ * Project Version: 3d5a 18-July-2014
  * Comments: CONTAINS EXAMPLE CODE AND IS NOT COMPLETE
  *			how to record movement information? (Eg recurrent network required) how to get synthetic network to modify its focus; left, right, in, out towards the objects of most interest?
  *******************************************************************************/
--- ANN/ANNsprite.cpp
+++ ANN/ANNsprite.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNsprite.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3d4a 08-June-2014
+ * Project Version: 3d5a 18-July-2014
  * Description: This code allows the addition of a sprite into a given scene file where a sprite is a paragraph of text. [The text is to be rendered in 3D, and point towards the user POV]
  *
  *******************************************************************************/
