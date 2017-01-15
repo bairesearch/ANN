66e3d90f5d19886f9051f1350197669d02849acd
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:38:57 +1100

    ANN 3i14b - move MU to ANNglobalDefs.h

--- ANN/ANNalgorithmBackpropagationUpdate.cpp
+++ ANN/ANNalgorithmBackpropagationUpdate.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNalgorithmBackpropagationUpdate.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 4a14a 16-June-2016
+ * Project Version: 4a14b 16-June-2016
  * Comments:
  *
  *******************************************************************************/
@@ -35,7 +35,6 @@
 #include "ANNalgorithmBackpropagationUpdate.h"
 #include "ANNneuronClass.h"
 
-#define	MU 0.1F
 
 
 int dataSetNum;
@@ -527,7 +526,7 @@ void calculateNewBiasOfNeuron(ANNneuron* neuronBeingAccessed)
 	#endif
 
 	double biasChange;
-	biasChange = MU* neuronBeingAccessed->error* 1.0;	//BIASES ARE WEIGHTED WIANNTH INPUT 1
+	biasChange = MU* neuronBeingAccessed->error* 1.0;	//BIASES ARE WEIGHTED WITH INPUT 1
 	neuronBeingAccessed->bias = neuronBeingAccessed->bias + biasChange;
 
 	#ifdef ANN_DEBUG
--- ANN/ANNglobalDefs.h
+++ ANN/ANNglobalDefs.h
@@ -26,7 +26,7 @@
  * File Name: ANNglobalsDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 4a14a 16-June-2016
+ * Project Version: 4a14b 16-June-2016
  * Description: ANN specific global definitions
  *
  * Usage Examples:
@@ -113,6 +113,10 @@
 	#endif
 #else
 	#define ANN_ALGORITHM_BACKPROPAGATION
+#endif
+
+#ifdef ANN_ALGORITHM_BACKPROPAGATION
+	#define	MU 0.1F		//backpropagation learning rate
 #endif
 
 
