b6f492a7c50b158c9620629b1e5374c5d672c530
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:37:47 +1100

    NO CHANGE RECORD

--- ANN/ANNparser.cpp
+++ ANN/ANNparser.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNparser.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3f7b 16-August-2015
+ * Project Version: 3f7c 16-August-2015
  * Comments: TH = Test Harness
  *
  *******************************************************************************/
