ed4249ab01d9ffe9d5c629b680979174826e581d
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:37:58 +1100

    NO CHANGE RECORD

--- ANN/ANNglobalDefs.h
+++ ANN/ANNglobalDefs.h
@@ -26,7 +26,7 @@
  * File Name: ANNglobalsDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3f7c 16-August-2015
+ * Project Version: 3f7d 16-August-2015
  * Description: ANN specific global definitions
  *
  * Usage Examples:
