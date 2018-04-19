0ddf01ef51c2fd5710c8e013d3501dcc11e12699
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:08:46 +1000

    NO CHANGE RECORD

--- ANN/ANNdraw.hpp
+++ ANN/ANNdraw.hpp
@@ -26,7 +26,7 @@
  * File Name: ANNdraw.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3m13c 22-February-2018
+ * Project Version: 3m13d 22-February-2018
  * Description: This code allows the addition of a sprite into a given scene file where a sprite is a paragraph of text. [The text is to be rendered in 3D, and point towards the user POV]
  * /
  *******************************************************************************/
