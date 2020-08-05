cac3a51d07fbbbd95c7b6a42f4e6c73748f8358b
Richard Baxter <richard.baxter@baxterai.com>
2020-08-06 02:07:27 +1000

    ANN 3n3c - update ANN_ALGORITHM_GIA_NEURAL_NETWORK preprocessor def comments ANN 3n3c - [sync with GIA3l1d] disable GIA_POS_REL_TRANSLATOR_SANI_ANN_COLOUR_CONNECTIONS_BASED_ON_COMPONENT_INDEX:ANN_ALGORITHM_GIA_NEURAL_NETWORK ANN_ALGORITHM_SEQUENCE_GRAMMAR_NETWORK_PRINT_COLOURS_COMPONENT_ORDER and GIA_POS_REL_TRANSLATOR_SANI_ANN_COLOUR_CONNECTIONS_BASED_ON_ACTIVATION:ANN_ALGORITHM_SEQUENCE_GRAMMAR_NETWORK_PRINT_COLOURS_ACTIVE

--- ANN/ANNdraw.hpp
+++ ANN/ANNdraw.hpp
@@ -26,7 +26,7 @@
  * File Name: ANNdraw.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3n3b 28-May-2020
+ * Project Version: 3n3c 28-May-2020
  * Description: This code allows the addition of a sprite into a given scene file where a sprite is a paragraph of text. [The text is to be rendered in 3D, and point towards the user POV]
  * /
  *******************************************************************************/
