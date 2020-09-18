a874a2c3ce1afcfccc0770723b8ec53bf25e0e08
Richard Baxter <richard.baxter@baxterai.com>
2020-09-18 20:58:20 +1000

    ANN 3n8a - create ANNtranslatorVariables (with firstInputNeuronInNetwork/firstOutputNeuronInNetwork from GIAtranslatorVariables) ANN 3n8a - rename all ANN_ALGORITHM_SEQUENCE_GRAMMAR from ANN_ALGORITHM_SANI_SEQUENCE_GRAMMAR ANN 3n8a - extract ANN_ALGORITHM_SANI_SEQUENCE_GRAMMAR from ANN_ALGORITHM_GIA_NEURAL_NETWORK ANN 3n8a - ANNneuron: rename all GIAneuronStrength to SANIneuronStrength, activationLevel to SANIactivationLevel ANN 3n8a - ANNneuronConnection: rename all GIAconnectionStrength to SANIconnectionStrength, GIAcomponentIndexFirst to SANIcomponentIndexFirst, GIAcomponentIndex to SANIcomponentIndex ANN 3n8a - create SANIneuronName

--- ANN/ANNxmlConversion.cpp
+++ ANN/ANNxmlConversion.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNxmlConversion.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3n7e 17-August-2020
+ * Project Version: 3n8a 09-September-2020
  * Comments
  * /
  *******************************************************************************/
