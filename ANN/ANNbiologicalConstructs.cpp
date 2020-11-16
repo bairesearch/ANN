ea46ed693dc3035ee4f6c10bf187d789222b7e2d
Richard Baxter <richard.baxter@baxterai.com>
2020-11-16 21:47:21 +1100

    NO CHANGE RECORD

--- ANN/ANNbiologicalConstructs.cpp
+++ ANN/ANNbiologicalConstructs.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNbiologicalConstructs.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3o2a 08-November-2020
+ * Project Version: 3o3a 16-November-2020
  * Comments: CONTAINS EXAMPLE CODE AND IS NOT COMPLETE
  *			how to record movement information? (Eg recurrent network required) how to get synthetic network to modify its focus; left, right, in, out towards the objects of most interest?
  * /
--- ANN/ANNdisplay.cpp
+++ ANN/ANNdisplay.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNdisplay.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3o2a 08-November-2020
+ * Project Version: 3o3a 16-November-2020
  * Comments: TH = Test Harness
  * /
  *******************************************************************************/
