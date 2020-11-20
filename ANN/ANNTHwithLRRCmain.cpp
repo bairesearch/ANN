56f68b9dcd5170579dbae681ee70d33e58fcc960
Richard Baxter <richard.baxter@baxterai.com>
2020-11-20 21:55:12 +1100

    NO CHANGE RECORD

--- ANN/ANNTHwithLRRCmain.cpp
+++ ANN/ANNTHwithLRRCmain.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNTHwithLRRCmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: Artificial Neural Network (ANN)
- * Project Version: 3o4b 17-November-2020
+ * Project Version: 3o4c 17-November-2020
  * Comments: TH = Test Harness
  * /
  *******************************************************************************/
