d47c7a9884cca012226e80acb5ea6de630dda84b
Richard Baxter <richard.baxter@baxterai.com>
2021-03-19 16:11:28 +1100

    ANN 3p3a - ANN_ALGORITHM_SANI_SEQUENCE_GRAMMAR: make ANNdraw.cpp use LRP_SHARED_ENTITY_TYPE_LOGIC_REFERENCE/LRP_SHARED_ENTITY_TYPE_LOGIC_REFERENCE_COLOUR

--- ANN/ANNdraw.cpp
+++ ANN/ANNdraw.cpp
@@ -26,7 +26,7 @@
  * File Name: ANNdraw.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2021 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3p2a 17-March-2021
+ * Project Version: 3p3a 19-March-2021
  * Description: This code allows the addition of a sprite into a given scene file where a sprite is a paragraph of text. [The text is to be rendered in 3D, and point towards the user POV]
  * /
  *******************************************************************************/
@@ -688,6 +688,10 @@ bool ANNdrawClass::ANNfillNeuronDisplayReference(LDreference* currentNeuronDispa
 		else if(neuron->SANIentityType == LRP_SHARED_ENTITY_TYPE_QUALITY)
 		{
 			col = LRP_SHARED_ENTITY_TYPE_QUALITY_COLOUR;
+		}
+		else if(neuron->SANIentityType == LRP_SHARED_ENTITY_TYPE_LOGIC_REFERENCE)
+		{
+			col = LRP_SHARED_ENTITY_TYPE_LOGIC_REFERENCE_COLOUR;
 		}
  		LDreferenceClassObject.convertLdrawColourToDatFileRGB(col, &colourrgb);
 		#else
